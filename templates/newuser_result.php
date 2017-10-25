a7130ed077d651432b235c10fdb38ca80d97b17a
Joost van Dijk <joost.vandijk@surfnet.nl>
2017-10-25 12:46:40 +0200

    include correct version of jQuery in enrolment page
    fixes an issue with some SimpleSAMLphp versions where enrolment does not automatically process after scanning an enrolment QR code.

--- templates/newuser_result.php
+++ templates/newuser_result.php
@@ -20,6 +20,11 @@
 
 $this->data['header'] = $this->t('{authTiqr:tiqr:header_enrollment}');
 $this->data['jquery'] = array('version' => '1.6', 'core' => true);
+if (sspmod_authTiqr_Helper_VersionHelper::useOldVersion()) {
+    $this->data['jquery'] = array('version' => '1.6', 'core' => true);
+} else {
+    $this->data['jquery'] = array('version' => '1.8', 'core' => true);
+}
 
 $this->includeAtTemplateBase('includes/header.php');
 
